fd12dd93458b8079ab5208dfba056abd83c99d59
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2018-02-23 19:11:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Kaamelott soundbot
+This is a Slack command for the [Kaamelott soundboard](https://kaamelott-soundboard.2ec0b4.fr).
+
+Search for a sound:
+```
+/kaamelott search sais
+```
+
+You will get a list of sounds that match your search:
+```
+4: Ah bah ça après, moi, pour le détail, je sais pas
+236: Mais si on fait marrer tout le monde avec nos chenilles à la purée de fraise, nos couilles d'oursin aux amandes, j'sais pas quelles autres saloperies là.
+263: J'vous disais que j'étais victime des colifichés et qu'il faudrait qu'on commence à me considérer en tant que tel.
+281: Bah je sais pas ? Me lâcher la grappe par exemple ?
+```
+
+Add a link to the sound in the current channel:
+```
+/kaamelott play 4
+```
+
+The following text will be posted:
+```
+https://kaamelott-soundboard.2ec0b4.fr/#son/apres_pour_le_detail_je_sais_pas
+```
+
+# Setup on your Slack
+Go to your Slack configuration page ```https://yourdomain.slack.com/apps/manage/custom-integrations```. Choose ```Slack commands```, then ```Add Configuration```.
+In the configuration page:
+* For ```Command```, choose the text you want to type use to invoke the command (/kaamelott seems to be a good match).
+* For ```URL```, enter you own instance of the bot or the existing instance ```https://kaamelott-soundbot.projects.matco.name```.
+* For ```Method``` choose "POST".
+* We don't care about the token.
+* For the rest, it's up to you.
+
+# Install your own instance on Google App Engine
+Checkout project from Github:
+```
+git clone git@github.com:matco/kaamelott-soundbot.git
+```
+
+Deploy to Google App Engine:
+```
+cd kaamelott-soundbot
+gcloud app deploy app.yaml
+```
+
+You're done!
+
+## Test the command
+If you would like to test your instance, here are some sample requests that will be sent by Slack:
+```
+curl -d "text=search%20sais" -H "Content-Type: application/x-www-form-urlencoded" -X POST http://your.instance
+curl -d "text=play%204" -H "Content-Type: application/x-www-form-urlencoded" -X POST http://your.instance
+```
---
+++ kaamelott.go
@@ -0,0 +1,158 @@
+package kaamelott
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/urlfetch"
+)
+
+//retrieve list of sounds from Github instead of Kaamelott soundboard website because their URL is regularly modified
+//const KaamelottSoundsURL = "https://kaamelott-soundboard.2ec0b4.fr/sounds/sounds.a7b9de88.json"
+const KaamelottSoundsURL = "https://raw.githubusercontent.com/2ec0b4/kaamelott-soundboard/master/sounds/sounds.json"
+
+//baselink of the sound on Kaamelott soundboard website
+const KaamelottSoundURL = "https://kaamelott-soundboard.2ec0b4.fr/#son/"
+
+type Sound struct {
+	Character string `json:"character"`
+	Episode   string `json:"episode"`
+	Title     string `json:"title"`
+	File      string `json:"file"`
+	Words     []string
+}
+
+type Match struct {
+	Index int
+	Sound *Sound
+}
+
+type SlackMessage struct {
+	ResponseType string `json:"response_type"`
+	Text         string `json:"text"`
+}
+
+var sounds []Sound
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func retrieve_sounds(ctx context.Context) {
+	client := urlfetch.Client(ctx)
+	//retrieve current sounds
+	resp, err := client.Get(KaamelottSoundsURL)
+	if err == nil {
+		json.NewDecoder(resp.Body).Decode(&sounds)
+		log.Infof(ctx, "Found %d Kaamelott sounds", len(sounds))
+
+		//build words list for each sounds to search efficiently
+		for i := 0; i < len(sounds); i++ {
+			sound := sounds[i]
+			sound.Words = strings.Split(sound.Title, " ")
+		}
+		log.Infof(ctx, "Retrieve all words related to sound")
+	} else {
+		log.Errorf(ctx, err.Error())
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		ctx := appengine.NewContext(r)
+		//build sounds cache if required
+		//this must be done in the context of a request, even if the cache will then be shared by all following requests
+		//use the first request to build this cache
+		if len(sounds) == 0 {
+			retrieve_sounds(ctx)
+		}
+		r.ParseForm()
+		//retrieve query stored in the "text" variable
+		text := r.Form.Get("text")
+		var query []string
+		if text != "" {
+			query = strings.Split(text, " ")
+		}
+		if len(query) > 0 {
+			//extract command with is first word of query
+			command := query[0]
+			//extract command arguments
+			arguments := query[1:]
+			log.Infof(ctx, "Executing command [%s] with arguments %s", command, arguments)
+			//executing command
+			switch command {
+			case "search":
+				if len(arguments) == 1 {
+					search := arguments[0]
+					log.Debugf(ctx, "Searching sound with search [%s]", search)
+					var matches []Match
+					//look for exact match
+					for i := 0; i < len(sounds) && len(matches) < 5; i++ {
+						sound := sounds[i]
+						for j := 0; j < len(sound.Words); j++ {
+							if search == sound.Words[j] {
+								matches = append(matches, Match{Index: i, Sound: &sound})
+							}
+						}
+					}
+					//look for approximate match
+					for i := 0; i < len(sounds) && len(matches) < 5; i++ {
+						sound := sounds[i]
+						//check that this sound as not already been selected
+						var selected = false
+						for j := 0; j < len(matches); j++ {
+							if matches[j].Sound == &sound {
+								selected = true
+								break
+							}
+						}
+						if !selected && strings.Contains(sound.Title, search) {
+							matches = append(matches, Match{Index: i, Sound: &sound})
+						}
+					}
+					log.Infof(ctx, "Found %d sounds", len(matches))
+					//transform matches into a multiple line string
+					message := ""
+					for i := 0; i < len(matches); i++ {
+						message += fmt.Sprintf("%v: %s\n", matches[i].Index, matches[i].Sound.Title)
+					}
+					var response = SlackMessage{ResponseType: "ephemeral", Text: message}
+					w.Header().Set("Content-Type", "application/json")
+					json.NewEncoder(w).Encode(response)
+				} else if len(arguments) > 1 {
+					fmt.Fprintf(w, "Only one search parameter is supported")
+				} else {
+					fmt.Fprintf(w, "Add search parameter")
+				}
+			case "play":
+				if len(arguments) > 0 {
+					id, err := strconv.Atoi(arguments[0])
+					if err == nil {
+						log.Infof(ctx, "Adding sound with id [%v]", id)
+						file := sounds[id].File
+						message := fmt.Sprintf("%s%s", KaamelottSoundURL, file[0:len(file)-4])
+						var response = SlackMessage{ResponseType: "in_channel", Text: message}
+						w.Header().Set("Content-Type", "application/json")
+						json.NewEncoder(w).Encode(response)
+					} else {
+						fmt.Fprint(w, "Wrong id")
+					}
+				} else {
+					fmt.Fprintf(w, "Add sound id")
+				}
+			default:
+				fmt.Fprintf(w, "Unsupported command %s", command)
+			}
+		} else {
+			fmt.Fprint(w, "Empty command")
+		}
+	} else {
+		fmt.Fprint(w, "Nothing here")
+	}
+}
